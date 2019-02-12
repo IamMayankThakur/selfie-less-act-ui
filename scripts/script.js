b6493ff848c288d0ef6d002641b63a19e5e46b42
nihali <nihalish26@gmail.com>
2019-02-13 01:53:11 +0530

    final

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ app.controller('testController', function ($scope, $http) {
   // $scope.home = "This is the homepage";
     $scope.getRequest = function () {
       console.log("I've been pressed!");
-      $http.get(ip + "/api/v1/categories/")
+      $http.get(ip + "/api/v1/categories")
         .then(function successCallback(response) {
           // $scope.home=response.data
           $scope.records = response.data
@@ -26,7 +26,7 @@ app.controller('testController', function ($scope, $http) {
         arr1.push(text);
         console.log(arr1)
 
-        $http.post(ip+"/api/v1/categories/",arr1)
+        $http.post(ip+"/api/v1/categories",arr1)
         .then(function success(){
          alert("category added");
          $scope.addcategory=""
@@ -48,7 +48,7 @@ app.controller('testController', function ($scope, $http) {
         arr1.push(text);
         console.log(arr1)
 
-        $http.delete(ip+"/api/v1/categories/"+text+'/')
+        $http.delete(ip+"/api/v1/categories/"+text)
         .then(function success(){
          alert("category deleted");
          $scope.deletecategory=""
@@ -61,7 +61,7 @@ app.controller('testController', function ($scope, $http) {
     $scope.init = function () {
       $scope.heading_categories = "Categories"
       
-      $http.get(ip + "/api/v1/categories/")
+      $http.get(ip + "/api/v1/categories")
         .then(function successCallback(response) {
           //  $scope.home1 = response.data
           
@@ -73,20 +73,36 @@ app.controller('testController', function ($scope, $http) {
 
     $scope.listacts = function () {
       $scope.acts_in_category = "acts in category:"
-      var text = $(this).id()
-      console.log("hello")
-      alert(text)
-      console.log(text)
-      $http.get(ip + "/api/v1/categories/"+ text + "/acts/")
+      var text = $(this).val("value")[0]
+      // for (key in text)
+      // {
+      //   if(key =="key")
+      //   {
+      //     console.log(key)
+      //   }
+      //    console.log(key)
+      // }
+      // for (let value of Object.values(text)) {
+        
+      //   console.log(value) // John, then 30
+      // }
+      for (const [key, value] of Object.entries(text)) {
+        if(key == "key"){
+          console.log(value)
+          var text2 = value
+          
+        }
+      }
+      
+      // console.log("hello")
+      // alert(text)
+      // console.log(text)
+      $http.get(ip + "/api/v1/categories/"+ text2 + "/acts")
         .then(function successCallback(response) {
           var resp = JSON.parse(response.data)
           console.log(resp)
           console.log(response.data)
-          // for(int i=0;i<resp.length;i++)
-          // {
 
-          // }
-          // $scope.home = response.data
           $scope.items = JSON.parse(response.data)
           
         }, function errorCallback(response) {
